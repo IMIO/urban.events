818ba6cbf7f25ac2b9854e0a602ab777cb32376b
Wiemboudabous <wiem.boudabous@imio.be>
2026-06-25 14:33:45 +0200

    Add dossi abandonne event for codt uniquelicence  envclassone and envclasstwo

--- src/urban/events/migrations.py
+++ src/urban/events/migrations.py
@@ -103,5 +103,28 @@ def import_free_notification_event(context):
             base_json_path="./profiles/config/standard/codt_article127",
             handle_existing_content=utils.ExistingContent.UPDATE,
         )
+    logger.info("upgrade done!")
 
+
+def install_dossier_abandonne_event(context):
+    logger.info("starting : Import dossier abandonne event for codt_uniquelicence")
+    if "liege" not in utils.get_configs():
+
+        # codt_uniquelicence
+        utils.import_all_config(
+            base_json_path="./profiles/config/standard/codt_uniquelicence",
+            handle_existing_content=utils.ExistingContent.UPDATE,
+        )
+
+        # envclassone
+        utils.import_all_config(
+            base_json_path="./profiles/config/standard/envclassone",
+            handle_existing_content=utils.ExistingContent.UPDATE,
+        )
+
+        # envclasstwo
+        utils.import_all_config(
+            base_json_path="./profiles/config/standard/envclasstwo",
+            handle_existing_content=utils.ExistingContent.UPDATE,
+        )
     logger.info("upgrade done!")
